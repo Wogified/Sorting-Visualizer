a24e25f3524d8cb9429becde0d82007907c62bd3
Wogified-Main <8029325+Wogified@users.noreply.github.com>
2020-05-11 20:29:59 -0400

    Updates

---
+++ precache-manifest.7146a1e06b4cfb4458383518cdd94464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbc7cfe7e1d7062c64dc9ba5510954e",
+    "url": "/sort_viz_app/index.html"
+  },
+  {
+    "revision": "aa9edf231d2c3b782d0e",
+    "url": "/sort_viz_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30764629bc8d41ccdc4b",
+    "url": "/sort_viz_app/static/js/2.6e34ddfa.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/sort_viz_app/static/js/2.6e34ddfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9edf231d2c3b782d0e",
+    "url": "/sort_viz_app/static/js/main.474b0cbf.chunk.js"
+  },
+  {
+    "revision": "03e2ad4e192dab6f0643",
+    "url": "/sort_viz_app/static/js/runtime-main.86c5bb1e.js"
+  }
+]);
